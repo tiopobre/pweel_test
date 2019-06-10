312552e0681ba5b369c0ccdcac4e3503262a447e
u1201736 <u1201736@unimilitar.edu.co>
2019-06-10 15:50:09 -0500

    kk

--- routes/index.js
+++ routes/index.js
@@ -26,10 +26,10 @@ router.get('/', function (req, res, next) {
 /* Obtener pagina de trabajos. */
 router.get('/trabajos', function (req, res, next) {
     // todas las vacantes
-    var obj;
+    var chunk1 = [];
+    var chunk2 = [];
     vacante.find({}, function (err, docs) {
-        var chunk1 = [];
-        var chunk2 = [];
+       
         for (var i = 0; i < docs.length; i++) {
             if (i % 2 == 0) {
                 chunk1.push(docs[i]);
@@ -37,16 +37,13 @@ router.get('/trabajos', function (req, res, next) {
                 chunk2.push(docs[i]);
             }
         }
-        obj = {
-            title: 'Trabajos | PWEEL',
-            style: 'style_trabajos.css',
-            vacantes1: chunk1,
-            vacantes2: chunk2,
-        };
     });
     //render pag
     res.render('trabajos', { 
-        obj
+        title: 'Trabajos | PWEEL',
+            style: 'style_trabajos.css',
+            vacantes1: chunk1,
+            vacantes2: chunk2,
     });
     //vacantes postuladas
 });
